b5750345ce4a1f667137622611d8a526a174d2a8
Laxmi Kanth Oruganti <oruganti.laxmikanth@gmail.com>
2025-05-17 11:11:05 -0500

    Comments added

--- Architecture/gem5_hello_world.py
+++ Architecture/gem5_hello_world.py
@@ -7,11 +7,14 @@ from gem5.isas import ISA
 from gem5.resources.resource import obtain_resource
 from gem5.simulate.simulator import Simulator
 
-
+# Create component objects
+# This example uses a simple board with a single core and no cache hierarchy
 cache_hierarchy = NoCache()
 memory = SingleChannelDDR3_1600("1GiB")
 processor = SimpleProcessor(cpu_type=CPUTypes.ATOMIC, num_cores=1, isa=ISA.X86)
 
+# Create a simple board with the components
+# The clock frequency is set to 3GHz
 board = SimpleBoard(
     clk_freq="3GHz",
     processor=processor,
@@ -19,8 +22,12 @@ board = SimpleBoard(
     cache_hierarchy=cache_hierarchy,
 )
 
+# Load a binary workload into the board
+# The binary is a simple hello world program compiled for x86_64
 binary = obtain_resource(resource_id="x86-hello64-static")
 board.set_se_binary_workload(binary)
 
+# Create a simulator object
+# The simulator will run the workload on the board
 simulator = Simulator(board=board)
 simulator.run()
